dd1f70017f0e628d98b44e8dc2fab4270d97531d
Diem-Trang Pham <phamdiemtrang1987@gmail.com>
2017-09-04 18:22:15 -0500

    Update makePDBfromUniqueModel.py

--- makePDBfromUniqueModel.py
+++ makePDBfromUniqueModel.py
@@ -1,8 +1,8 @@
 import os
 import sys
-import strip
-import analyze_models
-import makePDB
+from strip import *
+from analyze_models import *
+from makePDB import *
 
 if (len(sys.argv) < 4):
 	print ("Usage: filename_to_process node1,node1,...,node_n pdb_to_trim")
@@ -17,7 +17,7 @@ input_nodes = sys.argv[2]
 inputfile2 = sys.argv[3]
 
 # strip
-strip.Strip(inputfile, input_nodes)
+Strip(inputfile, input_nodes)
 
 select_nodes = input_nodes.strip().split(",")
 selected_nodes = []
@@ -28,9 +28,9 @@ for i in range(len(select_nodes)):
 filename = inputfile.replace(".sif", "-")+"-".join(selected_nodes)+"_py.txt"
 
 # strip analyze
-analyze_models.Strip_analyze(filename)
+Strip_analyze(filename)
 
 dir_unique_models = filename.replace(".txt", "")
 unique_models = os.path.join(dir_unique_models,"unique_sets_freq.txt")
 
-makePDB.makePDB(unique_models, inputfile2)
+makePDB(unique_models, inputfile2)
